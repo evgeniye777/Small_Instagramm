88906f9bb59d5863ee7e90a08b26bafff01dc8e3
evgeniye777 <evgeniye777.ru@mail.ru>
2021-03-04 18:10:18 +0300

    One_Commit

---
+++ app/src/main/java/comi/instasmall/small_instagramm/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package comi.instasmall.small_instagramm.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
